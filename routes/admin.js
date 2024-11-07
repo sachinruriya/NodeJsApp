5aca04fe262b8e3ea665894fd718b9edf49eafd8
sachinruriya <sachinruriya1999@gmail.com>
2024-11-07 22:50:01 +0530

    isauth var change

--- routes/admin.js
+++ routes/admin.js
@@ -5,8 +5,8 @@ const passport = require('passport');
 const {adminLoginPage,handleadminLogin,displayAdminPrf,editController, editProfiledata,deleteUser} = require("../controllers/adminController");
 const {adminAuthorization}= require("../middlewares/authorization.js")
 
-function isAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
+function isAuthen(req, res, next) {
+    if (req.isAuthen()) {
         return next();
     }
     res.status(401).send('Unauthorized');
@@ -16,16 +16,16 @@ router.post('/login', passport.authenticate('local', {
     failureRedirect: '/',
     failureMessage: true
 }), handleadminLogin);
-router.get("/dashboard",isAuthenticated,adminAuthorization(['admin']), displayAdminPrf);
+router.get("/dashboard",isAuthen,adminAuthorization(['admin']), displayAdminPrf);
 // router.get("/edit-userProfile/:id", isAuthenticated, editController);
-router.get("/edit-userProfile/:id", isAuthenticated, (req, res, next) => {
+router.get("/edit-userProfile/:id", isAuthen, (req, res, next) => {
     console.log("Received request for edit-userProfile with ID:");
     next(); // Calls the next middleware (editController in this case)
 }, editController);
-router.post("/profile-update/:id",isAuthenticated,(req,res,next) => {
+router.post("/profile-update/:id",isAuthen,(req,res,next) => {
  console.log("update", req);
  next();
 },editProfiledata)
-router.delete("/delete-user/:id",isAuthenticated,deleteUser)
+router.delete("/delete-user/:id",isAuthen,deleteUser)
 
 module.exports = router;
